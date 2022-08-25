e6b2c3f56c423737fd4a71fd37c7a9303253d72f
Carlos Izquierdo <izqunited@gmail.com>
2022-08-24 23:48:53 -0400

    Javadocs missing for class MetricPostServiceImpl.java.

--- src/main/java/cl/cizquierdonov/metrics/dal/services/impl/MetricPostServiceImpl.java
+++ src/main/java/cl/cizquierdonov/metrics/dal/services/impl/MetricPostServiceImpl.java
@@ -106,6 +106,14 @@ public class MetricPostServiceImpl implements MetricPostService {
     return average;
   }
 
+  /**
+   * Calculate post metric values average from specific date to one minute ahead for a specific metric type.
+   * @param type  Metric type name.
+   * @param calendarFrom  Date 'from' for average calculation.
+   * @param calendarTo  Date 'to' for average calculation.
+   * @param suffix  Concatenated at the end of the calculated average, to indicate the type of metric.
+   * @return   Average amount string with metric suffix at the end.
+   */
   private String calculateAveragePerMinute(String type, Calendar calendarFrom, Calendar calendarTo, String suffix) {
     String avgStr = null;
 
@@ -116,11 +124,17 @@ public class MetricPostServiceImpl implements MetricPostService {
     return avgStr;
   }
 
+  /**
+   * Calculate post metric values average from specific date to one hour ahead for a specific metric type.
+   * @param type  Metric type name.
+   * @param calendarFrom  Date 'from' for average calculation.
+   * @param calendarTo  Date 'to' for average calculation.
+   * @param suffix  Concatenated at the end of the calculated average, to indicate the type of metric.
+   * @return   Average amount string with metric suffix at the end.
+   */
   private String calculateAveragePerHour(String type, Calendar calendarFrom, Calendar calendarTo, String suffix) {
     String avgStr = null;
 
-    //calendarFrom.set(Calendar.MINUTE, 0);
-    //calendarTo.set(Calendar.MINUTE, 0);
     calendarTo.add(Calendar.HOUR, 1);
 
     avgStr = calculateAverage(type, calendarFrom, calendarTo, suffix);
@@ -128,26 +142,35 @@ public class MetricPostServiceImpl implements MetricPostService {
     return avgStr;
   }
 
+  /**
+   * Calculate post metric values average from specific date to one day ahead for a specific metric type.
+   * @param type  Metric type name.
+   * @param calendarFrom  Date 'from' for average calculation.
+   * @param calendarTo  Date 'to' for average calculation.
+   * @param suffix  Concatenated at the end of the calculated average, to indicate the type of metric.
+   * @return   Average amount string with metric suffix at the end.
+   */
   private String calculateAveragePerDay(String type, Calendar calendarFrom, Calendar calendarTo, String suffix) {
     String avgStr = null;
 
-    /*calendarFrom.set(Calendar.MINUTE, 0);
-    calendarTo.set(Calendar.MINUTE, 0);
-    calendarFrom.set(Calendar.HOUR, 0);
-    calendarTo.set(Calendar.HOUR, 0);*/
     calendarTo.add(Calendar.DAY_OF_MONTH, 1);
     avgStr = calculateAverage(type, calendarFrom, calendarTo, suffix);
 
     return avgStr;
   }
 
+  /**
+   * Get post metrics by type and by a date range from database and calculates a simply average.
+   * @param type  Metric type name to apply filter.
+   * @param calendarFrom  Date 'from' for average calculation.
+   * @param calendarTo  Date 'to' for average calculation.
+   * @param suffix  Concatenated at the end of the calculated average, to indicate the type of metric.
+   * @return  Average amount string with metric suffix at the end.
+   */
   private String calculateAverage(String type, Calendar calendarFrom, Calendar calendarTo, String suffix) {
     String avgStr;
     Timestamp from = new Timestamp(calendarFrom.getTime().getTime());
     Timestamp to = new Timestamp(calendarTo.getTime().getTime());
-    SimpleDateFormat sdf = new SimpleDateFormat(Constants.SECS_DATE_FORMAT);
-    LOG.info("##### from: " + sdf.format(calendarFrom.getTime()));
-    LOG.info("##### to: " + sdf.format(calendarTo.getTime()));
     List<MetricPost> entities = MetricPost.getAverageByDateRange(type, from, to);
     Double avg = 0.0;
 
